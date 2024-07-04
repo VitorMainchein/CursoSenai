463fa766470d85becaf2d8f3025b59bbafea8075
Vitor Mainchein <131178875+VitorMainchein@users.noreply.github.com>
2024-07-04 17:28:48 -0300

    curso

--- aula04.07.2027/ex001/index.js
+++ aula04.07.2027/ex001/index.js
@@ -1,7 +1,25 @@
 const express = require('express')
-
+const {Client} = require('pg')
+require("dotenv").config()
 const app = express()
+const client = new Client({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
 
+const connectDB = async () => {
+    client
+    .connect()
+    .then(() => {
+        console.log("A conexão funcioniou")
+    }).catch((err) => {
+        console.error('O erro ao conectar no Banco de dados')
+    })
+}
+connectDB()
 app.get('/test-api', function(req, res){
     res.send('Nossa API esta funcionando')
 })

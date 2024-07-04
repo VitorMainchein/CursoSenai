5934216f709ca929884bdbee22d843fc60ec6d48
VITOR MAINCHEIN MORALES <vitor_morales@estudante.sesisenai.org.br>
2024-07-04 16:29:06 -0300

    curso

--- aula04.07.2027/ex001/index.js
+++ aula04.07.2027/ex001/index.js
@@ -1,12 +1,9 @@
-function filtrarPares (arr){
-    const novoArr = []
-    for( let i = 0; i<arr.length; i++){
-        if(arr[i] % 2 == 0){
-            novoArr.push(arr[i])
-        }
-    }
-    return novoArr
-}
-arr = [1,2,3,4,5]
-let resultado = filtrarPares(arr)
-console.log(resultado)
+const express = require('express')
+
+const app = express()
+
+app.get('/test-api', function(req, res){
+    res.send('Nossa API esta funcionando')
+})
+
+app.listen(8000)
